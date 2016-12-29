b8af9fb2eaef7bb14554c553749b911bbf97268d
Henrik Melsom <melsom.henrik@gmail.com>
2016-12-29 05:21:11 +0100

    changed how wads are read
    reads data on a need to know basis

--- Assets/Scripts/MapBuilder.cs
+++ Assets/Scripts/MapBuilder.cs
@@ -2,14 +2,40 @@
 
 public class MapBuilder : MonoBehaviour
 {
-	public WAD wad;
+	bool inited = false;
+	public float scale = 1.0f / 64;
+	public MapData mapData;
 
 	// Use this for initialization
 	void Start()
 	{
-		wad = new WAD();
-		wad.loadWAD(Application.dataPath + "/Resources/DOOM.WAD");
-		wad.loadMap("E1M1", Application.dataPath + "/Resources/DOOM.WAD");
+		WADReader.loadWAD();
+		mapData = WADReader.loadMap("E1M1");
+
+		inited = true;
+	}
+
+	void OnDrawGizmos()
+	{
+		if (!inited) return;
+
+		Gizmos.color = Color.red;
+		foreach (MapData.Thing thing in mapData.things)
+		{
+			Gizmos.DrawWireCube(new Vector3(thing.x, 0, thing.y) * scale, Vector3.one * 10 * scale);
+		}
+		Gizmos.color = Color.blue;
+		foreach (MapData.Vertex vertex in mapData.vertexes)
+		{
+			Gizmos.DrawWireSphere(new Vector3(vertex.x, 0, vertex.y) * scale, 0.5f * 10 * scale);
+		}
+		Gizmos.color = Color.white;
+		foreach (MapData.Linedef linedef in mapData.linedefs)
+		{
+			MapData.Vertex startVertex = mapData.vertexes[linedef.start];
+			MapData.Vertex endVertex = mapData.vertexes[linedef.end];
+			Gizmos.DrawLine(new Vector3(startVertex.x, 0, startVertex.y) * scale, new Vector3(endVertex.x, 0, endVertex.y) * scale);
+		}
 	}
 
 	// Update is called once per frame
@@ -17,4 +43,35 @@ public class MapBuilder : MonoBehaviour
 	{
 
 	}
+}
+
+[System.Serializable]
+public class MapData
+{
+	public string name;
+	public Thing[] things;
+	public Linedef[] linedefs;
+	public Sidedef[] sidedefs;
+	public Vertex[] vertexes;
+	public Seg[] segs;
+	public SubSector[] subsectors;
+	public Node[] nodes;
+	public Sector[] sectors;
+
+	[System.Serializable]
+	public struct Thing { public int x, y, angle, type, options; }
+	[System.Serializable]
+	public struct Linedef { public int start, end, flags, types, tag, right, left; }
+	[System.Serializable]
+	public struct Sidedef { public int xofs, yofs, sector; public string texUpper, texLower, texMiddle; }
+	[System.Serializable]
+	public struct Vertex { public int x, y; }
+	[System.Serializable]
+	public struct Seg { public int start, end, angle, linedef, direction, offset; }
+	[System.Serializable]
+	public struct SubSector { public int num, start; }
+	[System.Serializable]
+	public struct Node { public int x, y, dx, dy, yul, yll, xul, xll, yur, ylr, xur, xlr, lft, rgt; }
+	[System.Serializable]
+	public struct Sector { public int floor, ceiling, light, special, tag; public string flatFloor, flatCeiling; }
 }
--- Assets/Scripts/WADReader.cs
+++ Assets/Scripts/WADReader.cs
@@ -2,110 +2,209 @@
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
 [System.Serializable]
-public static class WADReader
+public class WADReader
 {
-	static readonly string[] mapNamePatterns = { @"E\dM\d", @"MAP\d\d" };
-	static readonly string[] mapEntryNames = { "THINGS", "LINEDEFS", "SIDEDEFS", "VERTEXES", "SEGS", "SSECTORS", "NODES", "SECTORS", "REJECT", "BLOCKMAP" };
-
-	public static void loadWAD(this WAD wad, string pathName)
+	private static WADReader _instance;
+	public static WADReader instance
 	{
-		using (BinaryReader br = new BinaryReader(File.Open(pathName, FileMode.Open)))
+		get
 		{
-			// header
-			wad.type = Encoding.ASCII.GetString(br.ReadBytes(4));
-			wad.numLumps = br.ReadInt32();
-			wad.infoTableOfs = br.ReadInt32();
-
-			// directory
-			wad.directory = new WAD.LumpInfo[wad.numLumps];
-			br.BaseStream.Position = wad.infoTableOfs;
-			for (int i = 0; i < wad.directory.Length; i++)
+			if (_instance == null)
 			{
-				wad.directory[i].filePos = br.ReadInt32();
-				wad.directory[i].size = br.ReadInt32();
-				wad.directory[i].name = Encoding.ASCII.GetString(br.ReadBytes(8));
+				_instance = new WADReader();
+			}
+			return _instance;
+		}
+	}
+
+	public static string wadMode = "DOOM";
+	public static string iwadPath = "/Resources/DOOM.WAD";
+
+	public static LumpPointer[] lumpPointers;
+	public static Dictionary<string, int> mapLumps;
+
+	public static void loadWAD()
+	{
+		using (BinaryReader br = createBinaryReader(iwadPath))
+		{
+
+			string type = parseASCII(br.ReadBytes(4));
+			int numLumps = br.ReadInt32();
+			int dirPos = br.ReadInt32();
+
+			Debug.Log(string.Format("LOADING {0}, SIZE {1}, POS {2}", type, numLumps, dirPos));
+
+			lumpPointers = new LumpPointer[numLumps];
+			mapLumps = new Dictionary<string, int>();
+
+			br.BaseStream.Position = dirPos;
+			for (int i = 0; i < lumpPointers.Length; i++)
+			{
+				LumpPointer lump = new LumpPointer(iwadPath);
+				lump.pos = br.ReadInt32();
+				lump.size = br.ReadInt32();
+				// cleaning of string required since name is padded with zeroes if less than 8 bytes
+				lump.name = parseASCII(br.ReadBytes(8));
+				lumpPointers[i] = lump;
+
+				bool isMap = IsMap(lump.name);
+
+				// Debug.Log(string.Format("LUMP {0}, POS {1}, SIZE {2}, NAME {3}, MAP {4}", i, lump.pos, lump.size, lump.name, isMap.ToString().ToUpper()));
+
+				if (isMap)
+				{
+					mapLumps.Add(lump.name, i);
+				}
 			}
 		}
 	}
 
-	public static void loadMap(this WAD wad, string mapName, string pathName)
+	public static MapData loadMap(string mapName)
 	{
-		BinaryReader br = new BinaryReader(File.Open(pathName, FileMode.Open));
-		bool foundMap = false;
-		int index = 0;
-		for (int i = 0; i < wad.directory.Length; i++)
+		using (BinaryReader br = createBinaryReader(iwadPath))
 		{
-			if (string.Compare(wad.directory[i].name, mapName) == 0)
+			MapData md = new MapData();
+
+			int index = -1;
+			if (mapLumps.TryGetValue(mapName, out index))
 			{
-				wad.map.name = wad.directory[i].name;
-				foundMap = true;
-				index = i;
-				break;
-			}
-		}
-		if (foundMap)
-		{
-			bool foundNonMapEntry = false;
-			while (!foundNonMapEntry)
-			{
+				md.name = mapName;
+
 				index++;
-				if (IsMapEntry(wad.directory[index].name))
+				while (IsMapEntry(lumpPointers[index].name))
 				{
-					br.BaseStream.Position = wad.directory[index].filePos;
-					switch (wad.directory[index].name)
+					LumpPointer lump = lumpPointers[index];
+					br.BaseStream.Position = lump.pos;
+
+					switch (lump.name)
 					{
 						case "THINGS":
-							wad.map.things = new WAD.Map.Thing[wad.directory[index].size / 10];
-							for (int i = 0; i < wad.map.things.Length; i++)
+							md.things = new MapData.Thing[lump.size / 10];
+							for (int i = 0; i < md.things.Length; i++)
 							{
-								wad.map.things[i].x = br.ReadInt16();
-								wad.map.things[i].y = br.ReadInt16();
-								wad.map.things[i].angle = br.ReadInt16();
-								wad.map.things[i].type = br.ReadInt16();
-								wad.map.things[i].options = br.ReadInt16();
+								MapData.Thing thing = new MapData.Thing();
+								thing.x = br.ReadInt16();
+								thing.y = br.ReadInt16();
+								thing.angle = br.ReadInt16();
+								thing.type = br.ReadInt16();
+								thing.options = br.ReadInt16();
+								md.things[i] = thing;
 							}
 							break;
 						case "LINEDEFS":
+							md.linedefs = new MapData.Linedef[lump.size / 14];
+							for (int i = 0; i < md.linedefs.Length; i++)
+							{
+								MapData.Linedef linedef = new MapData.Linedef();
+								linedef.start = br.ReadInt16();
+								linedef.end = br.ReadInt16();
+								linedef.flags = br.ReadInt16();
+								linedef.types = br.ReadInt16();
+								linedef.tag = br.ReadInt16();
+								linedef.right = br.ReadInt16();
+								linedef.left = br.ReadInt16();
+								md.linedefs[i] = linedef;
+							}
+							break;
+						case "SIDEDEFS":
+							md.sidedefs = new MapData.Sidedef[lump.size / 30];
+							for (int i = 0; i < md.sidedefs.Length; i++)
+							{
+								MapData.Sidedef sidedef = new MapData.Sidedef();
+								sidedef.xofs = br.ReadInt16();
+								sidedef.yofs = br.ReadInt16();
+								sidedef.texUpper = parseASCII(br.ReadBytes(8));
+								sidedef.texLower = parseASCII(br.ReadBytes(8));
+								sidedef.texMiddle = parseASCII(br.ReadBytes(8));
+								sidedef.sector = br.ReadInt16();
+								md.sidedefs[i] = sidedef;
+							}
 							break;
 						case "VERTEXES":
+							md.vertexes = new MapData.Vertex[lump.size / 4];
+							for (int i = 0; i < md.vertexes.Length; i++)
+							{
+								MapData.Vertex vertex = new MapData.Vertex();
+								vertex.x = br.ReadInt16();
+								vertex.y = br.ReadInt16();
+								md.vertexes[i] = vertex;
+							}
+							break;
+						case "SEGS":
+							md.segs = new MapData.Seg[lump.size / 12];
+							for (int i = 0; i < md.segs.Length; i++)
+							{
+								MapData.Seg seg = new MapData.Seg();
+								seg.start = br.ReadInt16();
+								seg.end = br.ReadInt16();
+								seg.angle = br.ReadInt16();
+								seg.linedef = br.ReadInt16();
+								seg.direction = br.ReadInt16();
+								seg.offset = br.ReadInt16();
+								md.segs[i] = seg;
+							}
+							break;
+						case "SSECTORS":
+							md.subsectors = new MapData.SubSector[lump.size / 4];
+							for (int i = 0; i < md.subsectors.Length; i++)
+							{
+								MapData.SubSector subsector = new MapData.SubSector();
+								subsector.num = br.ReadInt16();
+								subsector.start = br.ReadInt16();
+								md.subsectors[i] = subsector;
+							}
+							break;
+						case "SECTORS":
+							md.sectors = new MapData.Sector[lump.size / 26];
+							for (int i = 0; i < md.sectors.Length; i++)
+							{
+								MapData.Sector sector = new MapData.Sector();
+								sector.floor = br.ReadInt16();
+								sector.ceiling = br.ReadInt16();
+								sector.flatFloor = parseASCII(br.ReadBytes(8));
+								sector.flatCeiling = parseASCII(br.ReadBytes(8));
+								sector.light = br.ReadInt16();
+								sector.special = br.ReadInt16();
+								sector.tag = br.ReadInt16();
+								md.sectors[i] = sector;
+							}
 							break;
 					}
-				}
-				else
-				{
-					foundNonMapEntry = true;
+
+					index++;
 				}
 			}
+
+			return md;
 		}
 	}
 
-	public static bool IsMapEntry(string lumpName)
-	{
-		bool result = false;
-		for (int i = 0; i < mapEntryNames.Length; i++)
-		{
-			if (lumpName == mapEntryNames[i])
-			{
-				result = true;
-				break;
-			}
-		}
-		return result;
-	}
+	static BinaryReader createBinaryReader(string path) { return new BinaryReader(File.Open(Application.dataPath+path, FileMode.Open)); }
 
-	public static bool IsMap(string lumpName)
+	// cleaning of string required since name is padded with zeroes if less than 8 bytes
+	static string parseASCII(byte[] bytes) { return new string(Encoding.ASCII.GetString(bytes).Where(char.IsLetterOrDigit).ToArray()); }
+
+	public static bool IsMap(string name) { return Regex.IsMatch(name, @"^(E\dM\d|MAP\d\d)"); }
+	public static bool IsMapEntry(string name) { return Regex.IsMatch(name, @"^(THINGS|LINEDEFS|SIDEDEFS|VERTEXES|SEGS|SSECTORS|NODES|SECTORS|REJECT|BLOCKMAP)"); }
+
+	public struct LumpPointer
 	{
-		bool result = false;
-		for (int i = 0; i < mapNamePatterns.Length; i++)
+		public string wad;
+		public int pos;
+		public int size;
+		public string name;
+
+		public LumpPointer(string wad)
 		{
-			if (Regex.IsMatch(lumpName, mapNamePatterns[i]))
-			{
-				result = true;
-				break;
-			}
+			this.wad = wad;
+
+			pos = -1;
+			size = -1;
+			name = "NULL";
 		}
-		return result;
 	}
 }
